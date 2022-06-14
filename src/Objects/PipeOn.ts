a6ceb895fbd9576c24302a35f642dc73317b5a63
luongminhduy <87934522+luongminhduy@users.noreply.github.com>
2022-06-14 17:09:45 +0700

    complepte Pipe objects

--- src/Objects/PipeOn.ts
+++ src/Objects/PipeOn.ts
@@ -1,19 +1,20 @@
 import { Bird } from "./Bird";
-
-export class Pipe {
+import { Pipe } from "./Pipe";
+export class PipeOn {
     x: number;
     y: number;
     img: string;
     bird: Bird;
     scene: Phaser.Scene;
     obtascle: any = 0;
-    frame: number = 0;
-    constructor(x: number, y: number, img: string, bird: Bird, scene: Phaser.Scene) {
-        this.x = x;
+    pipe: Pipe;
+    constructor(x: number, y: number ,img: string, bird: Bird, scene: Phaser.Scene, pipe: Pipe) {
+        this.x = x
         this.y = y;
         this.img = img;
         this.bird = bird;
         this.scene = scene;
+        this.pipe = pipe;
     }
     create() {
         this.obtascle = this.scene.physics.add.image(this.x, this.y, this.img);
@@ -24,8 +25,8 @@ export class Pipe {
     }
     update() {
         if (this.obtascle.x < - 20) {
-                this.obtascle.x = 300;
-                this.obtascle.y = Phaser.Math.Between(240, 500);
-        }
+            this.obtascle.x = 300;
+            this.obtascle.y = this.pipe.obtascle.y - 400;
+    }
     }
 }
--- src/scenes/GamePlaying.ts
+++ src/scenes/GamePlaying.ts
@@ -2,20 +2,21 @@ import Phaser from 'phaser'
 import { Base } from '~/Objects/Base';
 import { Bird } from '~/Objects/Bird';
 import { Pipe } from '~/Objects/Pipe';
-
+import { PipeOn } from '~/Objects/PipeOn';
 export default class GamePlaying extends Phaser.Scene {
     bird: Bird;
     base: Base;
     pipe: Pipe;
+    pipeOn: PipeOn;
     player: any;
     cursors: any;
-    pipeUnder: any;
 	constructor()
 	{
         super('flappy bird')
         this.bird = new Bird(0, 0, '', false, this, []);
         this.base = new Base(0, 0, '', this.bird, this);
-        this.pipe = new Pipe(0, 0, '', this.bird, this);
+        this.pipe = new Pipe(300, 500, '', this.bird, this);
+        this.pipeOn = new PipeOn(619, -100 ,'', this.bird, this, this.pipe);     
 	}
 
 	preload()
@@ -34,12 +35,14 @@ export default class GamePlaying extends Phaser.Scene {
     init() {
         this.bird = new Bird(40, 40, 'birdUp', false, this, ['birdUp', 'birdMid', 'birdDown']);
         this.base = new Base(140, 460, 'base', this.bird, this);
-        this.pipe = new Pipe(300, 400, 'pipeUnder', this.bird, this);
+        this.pipe = new Pipe(300, 800, 'pipeUnder', this.bird, this);
+        this.pipeOn = new PipeOn(619, -100, 'pipeOn', this.bird, this, this.pipe);
     }
     create () {
         this.cursors = this.input.keyboard.createCursorKeys();
         this.add.image(136, 256, 'background');   
         this.bird.create();
+        this.pipeOn.create();
         this.pipe.create();
         this.base.create();
 
@@ -47,6 +50,7 @@ export default class GamePlaying extends Phaser.Scene {
     update(time: number, delta: number): void {
         this.bird.update();
         this.pipe.update();
+        this.pipeOn.update();
         if (this.cursors.up.isDown) {
             this.bird.player.setVelocityY(-200);
             this.bird.player.angle = -45;
