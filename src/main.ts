42be2553fe743c583f91ca551211342501346af7
luongminhduy <87934522+luongminhduy@users.noreply.github.com>
2022-06-16 14:16:10 +0700

    Create Game Start Scene

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import Phaser, { Game } from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
 import GameTest from './scenes/GamePlaying'
 import GamePlaying from './scenes/GamePlaying'
 import GameOver from './scenes/GameOver'
+import GameStart from './scenes/GameStart'
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 280,
@@ -14,7 +14,7 @@ const config: Phaser.Types.Core.GameConfig = {
 			gravity: { y: 500 }
 		}
 	},
-	scene: [GameOver, GamePlaying]
+	scene: [GameStart, GameOver, GamePlaying]
 }
 
 export default new Phaser.Game(config)
---
+++ src/scenes/GameStart.ts
@@ -0,0 +1,47 @@
+import Phaser from 'phaser'
+import { Bird } from '~/Objects/Bird';
+import GamePlaying from './GamePlaying';
+export default class GameStart extends Phaser.Scene {
+    bird: Bird;
+    cursors: any;
+    img1: any = 0;
+    img2: any = 0;
+	constructor()
+	{
+        super('GameStart');
+        this.bird = new Bird(0, 0, '', false, this, []);
+	}
+
+	preload()
+    {
+        this.load.path = "BackGround/";
+        this.load.image('background', "bg.png");
+        this.load.image('message', 'message.png');
+        this.load.path = "Bird/";
+        this.load.image('birdUp', "redbird-upflap.png");
+        this.load.image('birdMid', "redbird-midflap.png");
+        this.load.image('birdDown', "redbird-downflap.png");
+    }
+    init() {
+        this.bird = new Bird(80, 305, 'birdUp', false, this, ['birdUp', 'birdMid', 'birdDown']);
+    }
+    create () {
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.img1 = this.physics.add.image(136, 256, 'background');
+        this.img1.body.allowGravity = false;
+        this.img1.setVelocityX(-20);
+        this.img2 = this.physics.add.image(336, 256, 'background');
+        this.img2.body.allowGravity = false;
+        this.img2.setVelocityX(-20);
+        this.add.image(136, 256, 'message');
+        this.bird.create();
+        this.bird.player.body.allowGravity = false;
+    }
+    update(time: number, delta: number): void {
+        if (this.cursors.up.isDown) {
+            this.scene.start('GamePlaying');
+        }
+        if (this.img1.x < - 60) this.img1.x = 336;
+        if (this.img2.x < - 60) this.img2.x = 336;
+    }
+}    
